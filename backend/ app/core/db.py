8b50a688d94f66b17295164e48ac9e6039e167b4
Dazzle-Trading <dazzletrading.syria@gmail.com>
2026-09-20 01:56:56 +0300

    Create db.py

---
+++ backend/ app/core/db.py
@@ -0,0 +1,9 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+DATABASE_URL = "postgresql://user:password@localhost:5432/dazzle_core"
+
+engine = create_engine(DATABASE_URL, echo=False)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
