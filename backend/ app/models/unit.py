a3e91cbeaaaefd5d07e68ebdb3163940d7b86432
Dazzle-Trading <dazzletrading.syria@gmail.com>
2026-09-20 01:58:55 +0300

    Create unit.py

---
+++ backend/ app/models/unit.py
@@ -0,0 +1,9 @@
+from sqlalchemy import Column, Integer, String
+from app.core.db import Base
+
+class Unit(Base):
+    __tablename__ = "units"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, index=True)
+    description = Column(String, nullable=True)
